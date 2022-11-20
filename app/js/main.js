cfcb741b06070cd729310a0ae09584f41a5fc6eb
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2022-11-20 22:25:29 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function(){
-   
-})
+const panel = document.querySelector(".main-left__panel")
+const panelBtn = document.querySelector(".header__button")
+
+// panelBtn.addEventListener("click",()=>{
+//     panel.classList.add('main-left__panel-active')
+//     if(panel.classList.contains('main-left__panel-active'))
+//         panel.classList.remove('main-left__panel-active')
+// });
+panelBtn.addEventListener("click",()=>{
+    if(panel.classList.contains('main-left__panel-active'))
+        panel.classList.remove('main-left__panel-active')
+    else{
+        panel.classList.add('main-left__panel-active')
+    }
+});
