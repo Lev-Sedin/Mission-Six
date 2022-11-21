0b4db2daf66f34be3b3d878686aff9bc17cf90bc
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2022-11-21 23:27:09 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,6 @@
 const panel = document.querySelector(".main-left__panel")
 const panelBtn = document.querySelector(".header__button")
 
-// panelBtn.addEventListener("click",()=>{
-//     panel.classList.add('main-left__panel-active')
-//     if(panel.classList.contains('main-left__panel-active'))
-//         panel.classList.remove('main-left__panel-active')
-// });
 panelBtn.addEventListener("click",()=>{
     if(panel.classList.contains('main-left__panel-active'))
         panel.classList.remove('main-left__panel-active')
