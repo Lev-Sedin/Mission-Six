31e7627d5615418424922c4f7d56587f531fbb71
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2022-12-01 22:24:58 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -3,87 +3,68 @@
 const panelUse = () =>{
     const panel = document.querySelector(".main-left__panel")
     const panelBtn = document.querySelector(".header__button")
+    const info = document.querySelector(".main-left__info")
+    console.log(info)
 
-    panelBtn.addEventListener("click",()=>{
-        if(panel.classList.contains('main-left__panel-active'))
+
+    panelBtn.addEventListener("click",function(){
+        if (panel.classList.contains('main-left__panel-active')){
+            info.classList.remove("main-left__info__active")
             panel.classList.remove('main-left__panel-active')
-        else{
+        }else{
             panel.classList.add('main-left__panel-active')
+            info.classList.add("main-left__info__active")
         }
-    });
+    })
 }
 
 panelUse()
 
 const tabFunc = () =>{
     const tabs = document.querySelectorAll(".continent-btn")
-    const tabsTitle = document.querySelector(".slider-box__name")
-    const tabsPrice = document.querySelector(".slider-box__price")
-    const tabsImage = document.querySelector(".slider-box__icon")
-    const tabsPlace = document.querySelector(".slider-box__country")
-    const orange = document.querySelector(".continent-icon__active")
-
-    const tabsOptions = [
-        {
-            name: "Italy",
-            price: "$2,500",
-            image: 'images/slider/europe/rome.jpeg',
-            place:"Rome",
-        },
-        {
-            name: "Phi Phi Islands",
-            price: "$1,480",
-            image: 'images/slider/indonesia/indonesia1.png',
-            place:"Thailand",
-        },
-        {
-            name: "New York",
-            price: "$3,200",
-            image: 'images/slider/america/ny.jpeg',
-            place:"America",
-        }
-        
-    ]
-
-    const changeContent = (index) => {
-        tabsTitle.textContent = `${tabsOptions[index].name}`
-        tabsPrice.textContent = `${tabsOptions[index].price}`
-        tabsImage.setAttribute("src", tabsOptions[index].image)
-        tabsPlace.textContent = `${tabsOptions[index].place}`
-    }
+    const infoTab = document.querySelectorAll('.slider-boxs')
 
 
-    const changeActiveTabs = ((indexClickedTab)=>{
-        tabs.forEach((tab, index) =>{
 
-            if(index === indexClickedTab){
-                tab.classList.add("continent-btn__active")
-            }else{
-                tab.classList.remove("continent-btn__active")
+
+    tabs.forEach(function(item){
+        item.addEventListener("click",function(){
+            let tab = item;
+            let tabId = tab.getAttribute("data-tab")
+            let tabZone = document.querySelector(tabId)
+
+
+            if(! tab.classList.contains("continent-btn__active")){
+                tabs.forEach(function(item){
+                    item.classList.remove('continent-btn__active');
+    
+                });
+                infoTab.forEach(function(item){
+                    item.classList.remove('slider-boxs__active');
+    
+                });
+    
+                tab.classList.add('continent-btn__active');
+                tabZone.classList.add('slider-boxs__active');
             }
-        })
-        changeContent(indexClickedTab)
-    })
 
-    tabs.forEach((tab, index) =>{
-        tab.addEventListener("click", ()=>{
-            changeActiveTabs(index);
+
         })
     })
 
-    changeContent(1)
+
 }
 
 tabFunc()
 
 const slaider = ()=>{
     let offset  = 0 //смещение от левого края
-    const slider = document.querySelector(".slider-boxs");
-    console.log(slider)
+    const slider = document.getElementById("tab-1");
+ 
 
     document.querySelector('.chapter-button__left').addEventListener("click",function(){
         offset = offset + 323;
-        if (offset > 323){
+        if (offset > 480){
             offset = 0
         }
         slider.style.left = -offset + "px"
@@ -91,10 +72,53 @@ const slaider = ()=>{
     document.querySelector('.chapter-button__right').addEventListener("click",function(){
         offset = offset - 323;
         if (offset < 0){
-            offset = 323
+            offset = 480
         }
         slider.style.left = -offset + "px"
     });
 }
-
 slaider()
+
+const slaider2 = ()=>{
+    let offset  = 0 //смещение от левого края
+    const slider = document.getElementById("tab-2");
+    console.log(slider)
+
+    document.querySelector('.chapter-button__left').addEventListener("click",function(){
+        offset = offset + 323;
+        if (offset > 480){
+            offset = 0
+        }
+        slider.style.left = -offset + "px"
+    });
+    document.querySelector('.chapter-button__right').addEventListener("click",function(){
+        offset = offset - 323;
+        if (offset < 0){
+            offset = 480
+        }
+        slider.style.left = -offset + "px"
+    });
+}
+slaider2()
+
+const slaider3 = ()=>{
+    let offset  = 0 //смещение от левого края
+    const slider = document.getElementById("tab-3");
+    console.log(slider)
+
+    document.querySelector('.chapter-button__left').addEventListener("click",function(){
+        offset = offset + 323;
+        if (offset > 480){
+            offset = 0
+        }
+        slider.style.left = -offset + "px"
+    });
+    document.querySelector('.chapter-button__right').addEventListener("click",function(){
+        offset = offset - 323;
+        if (offset < 0){
+            offset = 480
+        }
+        slider.style.left = -offset + "px"
+    });
+}
+slaider3()
