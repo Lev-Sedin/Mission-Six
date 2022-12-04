72bf245437672a12a03a3bd298220f3911ceef81
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2022-12-04 19:59:50 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 
 
+
 const panelUse = () =>{
     const panel = document.querySelector(".main-left__panel")
     const panelBtn = document.querySelector(".header__button")
@@ -122,3 +123,100 @@ const slaider3 = ()=>{
     });
 }
 slaider3()
+
+
+const diagramm = ()=>{
+    const ctx = document.getElementById("myChart").getContext("2d");
+
+
+        const labels=[
+            'Mon',
+            'Tue',
+            'Wed',
+            'Thu',
+            'Fri',
+            'Sat',
+            'Sun'
+        ];
+
+
+        const data = {
+            labels,
+            datasets:[
+                {
+                    data:[21,18,19,12,20,28,22],
+                    label: "Registered",
+                    backgroundColor:"#F86549",
+                    borderWidth:1,
+                    borderRadius:75,
+                    borderSkipped:false,
+                    barPercentage:0.3,
+                    
+                },
+                {
+                    data:[42,29,31,25,39,37,29],
+                    label: "Guest",
+                    backgroundColor:"#EDEFEF",
+                    borderWidth:1,
+                    borderRadius:75,
+                    borderSkipped:false,
+                    barPercentage:0.3,
+            },
+        ],
+        };
+
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {
+            plugins: {
+                legend:{
+                    display:true,
+                    align:"end",
+                    labels:{
+                        usePointStyle:true,
+                        pointStyle:'circle',
+                        display:true,
+                        color:"#8E9FB9",
+                        font:{
+                            size:16,
+                        },
+                    },
+                },
+            },
+            responsive: true,
+            interaction: {
+                intersect: false,
+            },
+            scales: {
+                x: {
+                    ticks:{
+                        color:"#AAB0C0"
+                    },
+                    stacked: true,
+                    grid:{
+                        display:false,
+                    }
+                },
+                y: {
+                    ticks:{
+                        color:"#AAB0C0",
+                        min: 10,
+                        max: 40,
+                        stepSize: 10
+                    },
+                    biginAtZero:true,
+                    stacked: false,
+                    grid:{
+                        color: "#F5F4F9",
+                    }
+                }
+            }
+            }
+        };
+
+    const myChart = new Chart(ctx, config)
+
+}
+
+diagramm()
--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ function images(){
 
 function scripts(){
     return src([
-        'node_modules/jquery/dist/jquery.js',
+        // 'node_modules/chart.js/dist/chart.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
